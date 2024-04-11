f5c64c6c2bdbf4781eaa516c9e4b0122cb2449e7
Sixten Arild Blumensaadt <siggerab@gmail.com>
2024-04-11 13:44:16 +0200

    Database structure made

---
+++ schema.sql
@@ -0,0 +1,36 @@
+drop table if exists user;
+CREATE TABLE user(
+    "id" INTEGER PRIMARY KEY,
+    'email' TEXT UNIQUE NOT NULL,
+    'password' TEXT NOT NULL
+);
+drop table if exists password;
+CREATE TABLE password(
+    "id" INTEGER PRIMARY KEY,
+    "user_id" INTEGER NOT NULL,
+    "title" TEXT UNIQUE,
+    "url" TEXT NOT NULL,
+    "username" TEXT,
+    "password" TEXT NOT NULL,
+    "note" TEXT,
+    "created" DATETIME NOT NULL,
+    "accessed" DATETIME,
+    "modified" DATETIME,
+    FOREIGN KEY ("user_id")
+        REFERENCES user ("id")
+);
+drop table if exists label;
+CREATE TABLE label(
+    "id" INTEGER PRIMARY KEY,
+    "user_id" INTEGER NOT NULL,
+    "name" TEXT,
+    FOREIGN KEY ("user_id")
+        REFERENCES user ("id")
+
+);
+drop table if exists association;
+CREATE TABLE association(
+    "label_id" INTEGER NOT NULL REFERENCES label(id),
+    "password_id" INTEGER NOT NULL REFERENCES password(id),
+    PRIMARY KEY (label_id, password_id)
+);
