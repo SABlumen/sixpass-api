986ad0a8c0e47147ab8e068b15bcfd12b1418d1d
SABlumen <siggerab@gmail.com>
2024-05-13 22:23:13 +0200

    Formatting

--- createdb.py
+++ createdb.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def create_db():
     db = sqlite3.connect("db.sqlite3")
     with open("schema.sql", "r") as f:
@@ -7,6 +8,7 @@ def create_db():
         print(sql)
     db.executescript(sql)
     db.commit()
+
 
 if __name__ == "__main__":
     create_db()
